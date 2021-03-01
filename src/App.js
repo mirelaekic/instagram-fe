526da8a05a7af3182368eef341a54c469dde73fa
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-03-01 14:01:42 +0100

    adds navbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Home from "./pages/Home/Home";
 import DM from "./pages/DM/DM";
 import Explore from "./pages/Explore/Explore";
 import Profile from "./pages/Profile/Profile"
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
+import NavBar from "./components/Navbar/NavBar"
 
 function App() {
   return (
-    <BrowserRouter>
+    <Switch>
+      <NavBar />
       <Route path="/" exact component={Home}/>
       <Route path="/dm" component={DM} />
       <Route path="/explore" component={Explore} />
       <Route path="/profile/:id" render={(props) => (<Profile {...props} />)} />
-    </BrowserRouter>
+    </Switch>
   );
 }
 
