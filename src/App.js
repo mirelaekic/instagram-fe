ea22ec8409cd89a4a7c08be65e96dd8531b5a32d
iSued <59610863+iSued@users.noreply.github.com>
2021-03-01 17:24:03 +0100

    footer done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import './App.css';
+import "./App.css";
 import Home from "./pages/Home/Home";
 import DM from "./pages/DM/DM";
 import Explore from "./pages/Explore/Explore";
-import Profile from "./pages/Profile/Profile"
-import { Switch, Route } from 'react-router';
-import NavBar from "./components/Navbar/NavBar"
+import Profile from "./pages/Profile/Profile";
+import { Switch, Route } from "react-router";
+import NavBar from "./components/Navbar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <Switch>
+    <>
       <NavBar />
-      <Route path="/" exact component={Home}/>
-      <Route path="/dm" component={DM} />
-      <Route path="/explore" component={Explore} />
-      <Route path="/profile/:id" render={(props) => (<Profile {...props} />)} />
-    </Switch>
+      <Switch>
+        <Footer />
+        <Route path="/" exact component={Home} />
+        <Route path="/dm" component={DM} />
+        <Route path="/explore" component={Explore} />
+        <Route path="/profile/:id" render={(props) => <Profile {...props} />} />
+      </Switch>
+    </>
   );
 }
 
