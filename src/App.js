5edd840c606a4141e173f36f48a69309697b7e6f
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-03-01 13:18:17 +0100

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/Home/Home";
+import DM from "./pages/DM/DM";
+import Explore from "./pages/Explore/Explore";
+import Profile from "./pages/Profile/Profile"
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home}/>
+      <Router path="/dm" component={DM} />
+      <Router path="/explore" component={Explore} />
+      <Route path="/profile/:id" render={(props) => (<Profile {...props} />)} />
+    </BrowserRouter>
   );
 }
 
