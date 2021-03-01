c9f02fdfa31e21079d973f392d709864ffd73a9c
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-03-01 13:31:55 +0100

    adds

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from "./pages/Home/Home";
 import DM from "./pages/DM/DM";
 import Explore from "./pages/Explore/Explore";
 import Profile from "./pages/Profile/Profile"
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Switch>
       <Route path="/" exact component={Home}/>
-      <Router path="/dm" component={DM} />
-      <Router path="/explore" component={Explore} />
+      <Route path="/dm" component={DM} />
+      <Route path="/explore" component={Explore} />
       <Route path="/profile/:id" render={(props) => (<Profile {...props} />)} />
-    </BrowserRouter>
+    </Switch>
   );
 }
 
