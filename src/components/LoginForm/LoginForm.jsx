0ff5c838758e0658390da8b9596a6060709ed2fb
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-03-04 16:11:07 +0000

    doing contact list for chat

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,60 +1,78 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
 import "../RegisterForm/RegisterForm.css";
-import { connect,useDispatch } from "react-redux";
-import { Redirect, withRouter} from "react-router-dom"
+import { connect, useDispatch } from "react-redux";
+import { Redirect, withRouter } from "react-router-dom";
 
 const mapStateToProps = (state) => state;
 
 const mapDispatchToProps = (dispatch) => ({
-  loginUser: (credentls) => 
-  dispatch({
-    type:"LOGIN_USER",
-    payload:credentls
-  }),
-  loginUserWithThunk: (credentls) => 
+  loginUser: (credentls) =>
+    dispatch({
+      type: "LOGIN_USER",
+      payload: credentls,
+    }),
+  loginUserWithThunk: (credentls) =>
     dispatch(async (dispatch, getState) => {
-      try { 
+      try {
         const response = await fetch(
           "http://localhost:9001/insta/users/login",
           {
             method: "POST",
-            headers: { "Content-Type": "application/json"},
-            credentials:"include", 
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
             body: JSON.stringify(credentls),
-          }, 
+          }
         );
         if (response.ok) {
+          const resp = await response.json();
           dispatch({
-            type:"LOGIN_USER",
-            payload:credentls
-          })
-          console.log(response, "login response")
+            type: "LOGIN_USER",
+            payload: resp,
+          });
+          console.log(response, "login response");
           localStorage.setItem("user", credentls.username);
         }
       } catch (error) {
         console.log(error);
       }
-    })
+    }),
 });
 
 const LoginForm = (props) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    props.loginUserWithThunk({username,password});
+    await props.loginUserWithThunk({ username, password });
+    props.history.push("/");
   };
 
   return (
     <Form onSubmit={handleSubmit}>
       <div className="reg-form mt-4">
-        <input required id="username" onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Username or username" />
-        <input type="password" required id="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        <input
+          required
+          id="username"
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          placeholder="Username or username"
+        />
+        <input
+          type="password"
+          required
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
       </div>
-      <button type="submit" className="reg-button">Log In</button> 
+      <button type="submit" className="reg-button">
+        Log In
+      </button>
     </Form>
   );
-}
-export default connect(mapStateToProps,mapDispatchToProps)(withRouter(LoginForm));
+};
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(LoginForm)
+);
--- src/pages/DM/DM.jsx
+++ src/pages/DM/DM.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { connect } from "react-redux";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
-export default function DM() {
-    return (
-        <div>
-            <h1>DM</h1>
-        </div>
-    )
+const connOpt = {
+  transports: ["websocket"],
+};
+
+let socket = io("http://localhost:9001", connOpt);
+
+const mapStateToProps = (state) => state;
+
+function DM(props) {
+  const [message, setMessage] = useState("");
+  const [allOnlineUsers, setAllOnlineUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState();
+  useEffect(() => {
+    socket.on("connect", () => console.log("connected to socket"));
+    socket.emit(
+      "VERIFY_USER",
+      props.loggedInUser.user.id.toString(),
+      (data) => {
+        setCurrentUser(data.user);
+      }
+    );
+    socket.on("USER_CONNECTED", (data) => setAllOnlineUsers(data));
+  }, []);
+
+  return (
+    <Container className="mt-5" id="chat-page">
+      <Row>
+        <Col xs={5} id="usersCol">
+          <ul>
+            <li>Global</li>
+            {allOnlineUsers.length > 0 &&
+              allOnlineUsers.map((user, i) => <li key={i}>{user.userId}</li>)}
+          </ul>
+        </Col>
+        <Col xs={7} id="chatCol">
+          <form id="chat">
+            <input
+              autoComplete="off"
+              value={message}
+              placeholder="Message"
+              onChange={(e) => setMessage(e.currentTarget.value)}
+            />
+            <Button type="submit" className="rounded-0">
+              Send
+            </Button>
+          </form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
+
+export default connect(mapStateToProps)(DM);
